a96b59e9006d4f81fce0227a318d0e95e82d30ae
GameCatCam <cameronrawlins11@gmail.com>
2023-07-17 12:05:50 -0400

    started the generatePassword function in javascript

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword creates the password to be displayed on the site
+function generatePassword() {
+  var charactersLower = "abcdefghijklmnopqrstuvwxyz"
+  var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var charactersNumber = "1234567890"
+  var charactersSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var passWord = ""
+
+  var characterSet = x
+  var passwordLength = window.prompt("Please input password length! \n\nNo less than 8 and no more than 128.")
+
+  var goku = window.confirm("Would you like to include lowercase letters?")
+  var gOku = window.confirm("Would you like to include uppercase letters?")
+  var goKu = window.confirm("Would you like to include numbers?")
+  var gokU = window.confirm("Would you like to include special characters?")
+
+  if (goku == true) {
+    if (gOku == true) {
+      
+    }
+  }
+
+  for (var i = 0; i <= passwordLength; i++) {
+    var funnyTime = Math.floor(Math.random() * charactersLower.length)
+    passWord += charactersLower.substring(funnyTime, funnyTime + 1)
+  }
+  
+}
